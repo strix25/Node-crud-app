cff38291170775375513fb33f58f8c26c6418ee9
strix <damjan.oslaj@gmail.com>
2019-01-01 15:54:39 +0100

    Express.js crud app
    .

---
+++ app.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require('path');
+const Joi = require('joi');
+
+const db  = require("./db");
+const collection = "todo";
+const app = express();
+
+const schema = Joi.object().keys({
+  todo : Joi.string().required()
+});
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req,res)=>{
+  db.getDB().collection(collection).find({}).toArray((err, documents)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log(documents);
+      res.json(documents);
+    }
+  });
+})
+
+app.put('/:id', (req, res)=>{
+  const todoID = req.params.id;
+  const userInput = req.body;
+
+  db.getDB().collection(collection).findOneAndUpdate({_id:db.getPrimaryKey(todoID)}, {$set: {todo:userInput.todo}}, {returnOriginal:false}, (err, result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json(result);
+    }
+  });
+
+});
+
+app.post('/',(req, res, next)=>{
+  const userInput = req.body;
+
+  Joi.validate(userInput,schema,(err,result)=>{
+    if(err){
+      const error = new Error("Invalid input");
+      error.status = 400;
+      next(error);
+    }else{
+      db.getDB().collection(collection).insertOne(userInput, (err,result)=>{
+        if(err){
+          const error = new Error("Failed to insert Todo Document");
+          error.status = 400;
+          next(error);
+        }else{
+          res.json({result: result, document: result.ops[0], msg: "Successfully inserted todo", error:null});
+        }
+      });
+    }
+  })
+
+  
+});
+
+app.delete('/:id', (req,res)=>{
+  const todoID = req.params.id;
+
+  db.getDB().collection(collection).findOneAndDelete({_id:db.getPrimaryKey(todoID)},(err,result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.json(result);
+    }
+  })
+});
+
+app.use((err,req,res,next)=>{
+  res.status(err.status).json({
+    error: {
+      message: err.message
+    }
+  })
+})
+
+db.connect((err)=>{
+  if(err){
+    console.log("unable to connect to db");
+    process.exit(1);
+  }else{
+    app.listen(3000, ()=>{
+      console.log("connected to db");
+    });
+  }
+})
